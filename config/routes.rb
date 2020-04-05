c372175f044c2b03991ce9d8fbb960d568f18b58
Joe Snyder <snyderjosephd@yahoo.com>
2020-04-05 14:44:30 -0500

    Added routes and controller for cats with index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cats, only: [:index, :show]
+  # resources :artworks, only: [:create, :destroy, :show, :update]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed the database with some cats
+Cat.destroy_all
+
+cat1 = Cat.create!(birth_date: '2015/01/20', color: 'White', name: 'Whiskers', sex: 'M')
+cat2 = Cat.create!(birth_date: '2017/02/17', color: 'Orange', name: 'Fluffy', sex: 'F')
+cat3 = Cat.create!(birth_date: '2014/07/19', color: 'Brown', name: 'Moe', sex: 'M', description: 'Moe is a nice cat who likes to play with humans.')
+cat3 = Cat.create!(birth_date: '2016/05/04', color: 'Gray', name: 'Curly', sex: 'F', description: 'Curly is an energetic cat who is not very nice and will steal your food if you are not careful.')
