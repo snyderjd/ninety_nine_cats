a17719915baa0de7285155ef904636990278d7bc
Joe Snyder <snyderjosephd@yahoo.com>
2020-04-12 16:29:13 -0500

    User can submit a cat_rental_request

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -2,11 +2,26 @@ class CatRentalRequestsController < ApplicationController
 
     # render the form for a new cat_rental_request
     def new
+        @cats = Cat.all
         render :new
     end
 
     # save a new cat_rental_request to the database
     def create
-
+        @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
+        
+        if @cat_rental_request.save
+            redirect_to cat_url(@cat_rental_request.cat)
+        else
+            flash.now[:errors] = @cat_rental_request.errors.full_messages
+            render :new
+        end
     end
+
+    private
+
+    def cat_rental_request_params
+        params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date, :status)
+    end
+
 end
